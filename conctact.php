5bea35666122c7f8b8b4d3ef37f9ea03ba06a3dd
Jose Lopez <leitto10@gmail.com>
2019-01-25 22:16:58 -0800

    edit files

--- conctact.php
+++ conctact.php
@@ -1,94 +1,34 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
-
 <?php
-// define variables and set to empty values
-$nameErr = $emailErr = "";
-$name = $email = "";
+$nameErr = $emailErr = $phoneErr = $stateErr = "";
+$name = $email = $phone = $state = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-
-    $tel = '091-9869123456';
-
-    if(preg_match("/^[091]{3}-[0-9]{10}$/", $tel)) {
-        echo "valid";
-    } else {
-        echo "invalid";
-    }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
+ function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  if (empty($_GET["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_GET["name"]);
+  }
+
+  if (empty($_GET["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_GET["email"]);
+  }
+}
+ 
+
+ echo $_GET["name"]; 
+ echo $_GET["email"];
+echo $_GET["phone"]; 
+ echo $_GET["state"]; 
+
+
+
 ?>
-
-<h2>Contact Jose</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Phone#: <input type="tel" name="phone" id="phone">
-  <span class="error"><?php echo $pone;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $phone;
-echo "<br>";
-echo $comment;
-?>
-
-</body>
-</html>
